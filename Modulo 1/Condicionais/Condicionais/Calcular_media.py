7b07194d79902a9f312635f91ab8be9e74f1083c
André Santos <ofirbrb@gmail.com>
2025-11-18 12:47:20 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/Condicionais/Calcular_media.py
@@ -0,0 +1,9 @@
+nota1 = float(input('Digite a sua 1° nota:'))
+nota2 = float(input('Digite a sua 2° nota:'))
+nota3 = float(input('Digite a sua 3° nota:'))
+nota4 = float(input('Digite a sua 4° nota:'))
+
+soma = nota1 + nota2 + nota3 + nota4
+media = soma/4
+
+print('Sua media final foi de:', media)
