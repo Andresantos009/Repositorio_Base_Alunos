2ddb9bf43d2ad1e5b32e6b39d3283d02446ce237
André Santos <ofirbrb@gmail.com>
2025-11-27 15:45:24 -0300

    Add files via upload

---
+++ Modulo 2/Banco de Dados/Banco de Dados/Projeto_08_Banco_Dados_Filmes-main/selects_filmes.sql
@@ -0,0 +1,78 @@
+-- Mostrando a Tabela toda 
+SELECT * FROM filmes
+
+
+-- 1. Filmes com melhor avaliação
+SELECT titulo, diretor, nota_imdb 
+FROM filmes 
+ORDER BY nota_imdb DESC 
+LIMIT 10;
+
+-- 2. Filmes por década
+SELECT 
+    (ano_lancamento / 10) * 10 as decada,
+    COUNT(*) as total_filmes,
+    AVG(nota_imdb) as media_nota
+FROM filmes 
+GROUP BY decada 
+ORDER BY decada;
+
+-- 3. Diretores com mais filmes na lista
+SELECT diretor, COUNT(*) as total_filmes, AVG(nota_imdb) as media_nota
+FROM filmes 
+GROUP BY diretor 
+HAVING COUNT(*) > 1
+ORDER BY total_filmes DESC;
+
+-- 4. Gêneros mais populares
+SELECT genero, COUNT(*) as total, AVG(nota_imdb) as media_nota
+FROM filmes 
+GROUP BY genero 
+ORDER BY total DESC;
+
+-- 5. Filmes mais longos
+SELECT titulo, duracao_minutos, genero
+FROM filmes 
+ORDER BY duracao_minutos DESC 
+LIMIT 10;
+
+-- 6. Eu quero saber quais são os filmes do Diretor Christopher Nolan
+SELECT * from filmes where diretor = "Christopher Nolan"
+
+-- 7. Eu quero saber quais são os filmes do James Gunn
+SELECT * from filmes where diretor = "James Gunn"
+
+-- 8. Eu quero saber quais os filmes de ação lançados em 2016
+SELECT * from filmes where genero = "Ação" and ano_lancamento = 2016
+
+-- 9. Eu quero saber quais os filmes do batman e quais os diretores
+SELECT diretor, titulo from filmes where titulo like "%Batman%"
+
+-- 10. Eu quero saber quais os filmes de terror e drama lançado entre 1980 e 2002
+SELECT * from filmes where genero in ("Terror","Drama")
+and ano_lancamento BETWEEN 1980 and 2002 order by ano_lancamento
+
+-- 11. Eu quero Saber quantos filmes foram lançados em 1999
+SELECT count(titulo) as total_filmes from filmes where ano_lancamento = 1999
+
+-- 12. Eu quero saber quantos filmes tem o diretor Quentin Tarantino
+SELECT * from filmes where diretor = "Quentin Tarantino"
+
+-- 13. Eu quero saber quais os filmes do James Cameron e quais os generos deles
+SELECT * from filmes where diretor = "James Cameron" and genero
+
+-- 14. Por fim vamos adiconar as informações do seu filme favorito na tabela.
+INSERT into filmes(titulo, genero, ano_lancamento, duracao_minutos, diretor, nota_imdb)VALUES
+('Até o Último Homem', 'Drama', '2016', '139', 'Mel Gibson', 8.1)
+
+-- 15. Vamos adiconar as informações do filme que o professor odeia.
+INSERT into filmes(titulo, genero, ano_lancamento, duracao_minutos, diretor, nota_imdb)VALUES
+(' Homem-Aranha: Longe de Casa', 'Ação', 2019 , 129 , 'Jon Watts', 7.3)
+
+-- 16. Vamos deletar esse filme que o professor detesta.
+DELETE filmes(titulo, genero, ano_lancamento, duracao_minutos, diretor, nota_imdb)VALUES
+('Homem-Aranha: Longe de Casa', 'Ação', 2019 , 129 , 'Jon Watts', 7.3)
+
+
+
+
